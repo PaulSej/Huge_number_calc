35875fe0db304ee0d5f2dc19ee42dcb8eb1b948e
rukshiniRasa <62998600+rukshiniRasa@users.noreply.github.com>
2020-11-20 16:54:17 +0100

    Update HugeUnsignedInt.c

--- HugeNumberCalculator/UseHugeNumber/Type/HugeUnsignedInt.c
+++ HugeNumberCalculator/UseHugeNumber/Type/HugeUnsignedInt.c
@@ -1,165 +1,44 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "HugeUnsignedIntOperator.h"
+#include "HugeUnsignedInt.h"
 
-#define HUGE_NUMBER_SUPERIOR 1
-#define HUGE_NUMBER_EQUAL 2
-#define HUGE_NUMBER_INFERIOR 3
-
-static unsigned int compareHugeUnsignedInts (const HugeUnsignedInt* operand1, const HugeUnsignedInt* operand2);
-
-HugeUnsignedInt* addHugeUnsignedInt (const HugeUnsignedInt* operand1, const HugeUnsignedInt* operand2) {
-    HugeUnsignedInt* result = createHugeUnsignedInt ();
-
-    if (result != NULL) {
-        Node* node1 = operand1->end;
-        Node* node2 = operand2->end;
-        Digit carry = 0;
-        while (!isNodeEmpty (node1) || !isNodeEmpty (node2)) {
-            Digit digitNode1 = isNodeEmpty (node1) ? 0 : node1->digit;
-            Digit digitNode2 = isNodeEmpty (node2) ? 0 : node2->digit;
-            Digit digitResult = (digitNode1 + digitNode2 + carry) % 10;
-            carry = (digitNode1 + digitNode2 + carry) / 10;
-            addDigitAtStart (result, digitResult);
-            if (!isNodeEmpty (node1)) {
-                node1 = node1->previous;
-            }
-            if (!isNodeEmpty (node2)) {
-                node2 = node2->previous;
-            }
-        }
-        if (carry > 0) {
-            addDigitAtStart (result, carry);
-        }
-    }
-    return result;
+HugeUnsignedInt* createHugeUnsignedInt (void) {
+    return createDoublyLinkedList ();
 }
 
-HugeInt* substractHugeUnsignedInt (const HugeUnsignedInt* operand1, const HugeUnsignedInt* operand2) {
-    HugeInt* result = createHugeInt ();
+HugeUnsignedInt* createHugeUnsignedIntFromString (const char* digitString) {
+    return simplifyHugeUnsignedInt (createDoublyLinkedListFromString (digitString));
+}
 
-    if (result != NULL) {
-        Node* node1;
-        Node* node2;
-        Digit carry = 0;
-
-        if (isHugeUnsignedIntSuperior (operand1, operand2)) {
-            node1 = operand1->end;
-            node2 = operand2->end;
-            result->sign = PLUS;
-        } else {
-            node1 = operand2->end;
-            node2 = operand1->end;
-            result->sign = MINUS;
+HugeUnsignedInt* createHugeUnsignedIntFromHugeUnsignedInt (const HugeUnsignedInt* hugeUnsignedInt, const unsigned int size) {
+    HugeUnsignedInt* hugeUnsignedIntCopy = createHugeUnsignedInt ();
+    if ((hugeUnsignedIntCopy != NULL) && (hugeUnsignedInt != NULL)) {
+        Node* node = hugeUnsignedInt->start;
+        for (unsigned int i = 0; ((i < size) || (size == 0)) && !isNodeEmpty (node); i++) {
+            addDigitAtEnd (hugeUnsignedIntCopy, node->digit);
+            node = node->next;
         }
+    }
+    return hugeUnsignedIntCopy;
+}
 
-        while (!isNodeEmpty (node1) || !isNodeEmpty (node2)) {
-            Digit digitResult;
-            Digit digitNode1 = isNodeEmpty (node1) ? 0 : node1->digit;
-            Digit digitNode2 = isNodeEmpty (node2) ? 0 : node2->digit;
-            if (((digitNode1 == 0) && (carry > 0)) || ((digitNode1 - carry) < digitNode2)) {
-                digitNode1 += 10 - carry;
-                carry = 1;
+void deleteHugeUnsignedInt (HugeUnsignedInt* hugeUnsignedInt) {
+    deleteDoublyLinkedList (hugeUnsignedInt);
+}
+
+int getHugeUnsignedIntLength (const HugeUnsignedInt* hugeUnsignedInt) {
+    return getDoublyLinkedListLength (hugeUnsignedInt);
+}
+
+HugeUnsignedInt* simplifyHugeUnsignedInt (HugeUnsignedInt* hugeUnsignedInt) {
+    if (hugeUnsignedInt != NULL) {
+        char simplify = 1;
+        while ((!isNodeEmpty (hugeUnsignedInt->start)) && simplify) {
+            if ((hugeUnsignedInt->start->digit == 0) && hugeUnsignedInt->start != hugeUnsignedInt->end) {
+                removeStartFromDoublyLinkedList (hugeUnsignedInt);
             } else {
-                digitNode1 -= carry;
-                carry = 0;
-            }
-            digitResult = (digitNode1 - digitNode2) % 10;
-            addDigitAtStart (result->absoluteValue, digitResult);
-            if (!isNodeEmpty (node1)) {
-                node1 = node1->previous;
-            }
-            if (!isNodeEmpty (node2)) {
-                node2 = node2->previous;
+                simplify = 0;
             }
         }
     }
-    simplifyHugeInt (result);
-    return result;
-}
-
-HugeUnsignedInt* multiplyHugeUnsignedInt (const HugeUnsignedInt* operand1, const HugeUnsignedInt* operand2) {
-    if ((operand1 == NULL) || (operand2 == NULL)) {
-        return NULL;
-    }
-    HugeUnsignedInt* result = createHugeUnsignedInt ();
-    if (result == NULL) {
-        return NULL;
-    }
-    unsigned int column = 0;
-    for (Node* node2 = operand2->end; !isNodeEmpty (node2); node2 = node2->previous, column++) {
-        Digit carry = 0;
-        HugeUnsignedInt* temp = createHugeUnsignedInt ();
-        if (temp == NULL) {
-            return NULL;
-        }
-        for (Node* node1 = operand1->end; !isNodeEmpty (node1); node1 = node1->previous) {
-           Digit digitNode1 = node1->digit;
-            Digit digitNode2 = node2->digit;
-            Digit digitResult = ((digitNode1 * digitNode2) + carry) % 10;
-            carry = ((digitNode1 * digitNode2) + carry) / 10;
-            addDigitAtStart (temp, digitResult);
-        }
-        if (carry > 0) {
-            addDigitAtStart (temp, carry);
-        }
-        for (unsigned int i = 0; i < column; i++) {
-            addDigitAtEnd (temp, 0);
-        }
-        HugeUnsignedInt* tmpResult = addHugeUnsignedInt (result, temp);
-        deleteHugeUnsignedInt (result);
-        result = tmpResult;
-        deleteHugeUnsignedInt (temp);
-    }
-
-    simplifyHugeUnsignedInt (result);
-    return result;
-}
-
-HugeUnsignedInt* divideHugeUnsignedInt (const HugeUnsignedInt* operand1, const HugeUnsignedInt* operand2) {
-    if ((operand1 == NULL) || (operand2 == NULL)) {
-        return NULL;
-    }
-    HugeUnsignedInt* result = createHugeUnsignedIntFromString ("0");
-    if (result == NULL) {
-        return NULL;
-    }
-    unsigned int operand1Length = getHugeUnsignedIntLength (operand1);
-    unsigned int operand2Length = getHugeUnsignedIntLength (operand2);
-    unsigned int place = operand2Length;
-    HugeInt* remainder = createHugeInt ();
-    remainder->absoluteValue = createHugeUnsignedIntFromHugeUnsignedInt (operand1, operand2Length);
-    while (place <= operand1Length) {
-        HugeInt* keepForDelete;
-        HugeUnsignedInt* quotient;
-        HugeUnsignedInt* multiplyResult;
-        quotient = createHugeUnsignedIntFromString ("1");
-        multiplyResult = multiplyHugeUnsignedInt (quotient, operand2);
-        unsigned int compareResult = compareHugeUnsignedInts (remainder->absoluteValue, multiplyResult);
-        while (compareResult == HUGE_NUMBER_SUPERIOR) {
-            deleteHugeUnsignedInt (multiplyResult);
-            incrementHugeUnsignedInt (quotient);
-            multiplyResult = multiplyHugeUnsignedInt (quotient, operand2);
-            compareResult = compareHugeUnsignedInts (remainder->absoluteValue, multiplyResult);
-        }
-        keepForDelete = remainder;
-        if (compareResult == HUGE_NUMBER_INFERIOR) {
-            decrementHugeUnsignedInt (quotient);
-            deleteHugeUnsignedInt (multiplyResult);
-            multiplyResult = multiplyHugeUnsignedInt (quotient, operand2);
-        }
-        addDigitAtEnd (result, quotient->start->digit);
-        remainder = substractHugeUnsignedInt (remainder->absoluteValue, multiplyResult);
-
-        deleteHugeUnsignedInt (multiplyResult);
-        deleteHugeInt (keepForDelete);
-        deleteHugeUnsignedInt (quotient);
-        if (place < operand1Length) {
-            addDigitAtEnd (remainder->absoluteValue, getDigitByPlaceFromStart (operand1, place));
-            simplifyHugeInt (remainder);
-        }
-        place++;
-    }
-    simplifyHugeUnsignedInt (result);
-    return result;
+    return hugeUnsignedInt;
 }
